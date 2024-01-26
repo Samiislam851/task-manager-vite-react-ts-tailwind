f3f86bf2d71c052142f0b8b60f06352e36a919a8
siam <samisiam851@gmail.com>
2024-01-26 20:43:26 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let a: number;
+  a = 5
+  console.log(a);
+
+
+  return (
+    <div className="font-saira">
+
+<h1 className="text-3xl">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
