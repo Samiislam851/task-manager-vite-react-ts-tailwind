1aacca90209a1f6d63a743cb850814f46d0f7bab
siam <samisiam851@gmail.com>
2024-01-27 02:36:31 +0600

    delete functionality done

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,25 @@ import TaskCard from './components/TaskCard/TaskCard'
 
 function App() {
   interface taskType {
+    id: number,
     task: string,
     dateAndTime: Date,
     isDone: boolean
   }
+
+
+  function generateUniqueId(objectsArray: taskType[]) {
+    let id: number;
+    do {
+      // Generate a random number and convert it to a hexadecimal string
+      id = Math.floor(Math.random() * 10000)
+      // Check if the generated ID already exists in any object's ID
+    } while (objectsArray.some(obj => obj.id === id));
+    return id;
+  }
+
+
+
 
   const [taskText, setTaskText] = useState<string>('')
   const [tasks, setTasks] = useState<taskType[]>([])
@@ -19,10 +34,18 @@ function App() {
     e.preventDefault()
     console.log(taskText, tasks);
     if (taskText) {
-      let temp: taskType[] = [...tasks, { task: taskText, dateAndTime: new Date(), isDone: false }]
+      let generatedId = generateUniqueId(tasks)
+
+      let temp: taskType[] = [...tasks, { id: generatedId, task: taskText, dateAndTime: new Date(), isDone: false }]
       setTasks(temp)
       setTaskText('')
     }
+  }
+  const handleDeleteTask = (id: number) => {
+    console.log('clicked!',id);
+    
+    const newTasks = tasks.filter(task => task.id !== id)
+    setTasks(newTasks)
   }
 
   return (
@@ -36,13 +59,13 @@ function App() {
       <InputField taskText={taskText} handleAddTask={handleAddTask} setTaskText={setTaskText} />
       <div className='flex flex-col md:flex-row px-10'>
         <div className='basis-1/2'>
-          <h3 className='py-2 text-center text-xl'>{tasks[0]&&'To Do :'} </h3>
-          {tasks.map((taskData: taskType, i: number) => <TaskCard key={i} taskData={taskData} />)}
+          <h3 className='py-2 text-center text-xl'>{tasks[0] && 'To Do :'} </h3>
+          {tasks.map((taskData: taskType, i: number) => <TaskCard handleDeleteTask={handleDeleteTask} key={i} taskData={taskData} />)}
         </div>
-        <div className='basis-1/2'>
-          <h3 className='py-2 text-center text-xl'>{tasks[0]&&'Done :'} </h3>
+        {/* <div className='basis-1/2'>
+          <h3 className='py-2 text-center text-xl'>{tasks[0] && 'Done :'} </h3>
           {tasks.map((taskData: taskType, i: number) => <TaskCard key={i} taskData={taskData} />)}
-        </div>
+        </div> */}
       </div>
 
 
--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 interface taskType {
+    id: number,
     task: string,
     dateAndTime: Date,
-    isDone: boolean
+    isDone: boolean,
+
 }
-const TaskCard = ({ taskData }: { taskData: taskType }) => {
-    const { task, dateAndTime, isDone } = taskData;
-    console.log(task, dateAndTime, isDone);
+
+interface props {
+    taskData: taskType,
+    handleDeleteTask: (id: number) => void
+}
+
+const TaskCard = ({ taskData, handleDeleteTask }: props) => {
+    const { id, task, dateAndTime, isDone } = taskData;
+    console.log(id, task, dateAndTime, isDone);
 
     return (
         <div className='flex justify-around hover:shadow-xl transition-all duration-300 ease-in-out p-3 bg-gray-100 rounded-lg my-2 mx-3 text-gray-500 '>
             <div>{task}</div>
-            <div>{isDone?'Done':'Not Done'}</div>
-            <div><button className='p-1 px-3 text-white text-base rounded-xl hover:scale-105 transition-all duration-300 ease-in-out bg-red-600'>Delete</button></div>
+            <div>{isDone ? 'Done' : 'Not Done'}</div>
+            <div><button onClick={()=>handleDeleteTask(id)} className='p-1 px-3 text-white text-base rounded-xl hover:scale-105 transition-all duration-300 ease-in-out bg-red-600'>Delete</button></div>
         </div>
     );
 };
